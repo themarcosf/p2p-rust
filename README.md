9b03b55756e9dc38f7a35de0aa3286b9fde8c0ec
Marcos F <84358344+themarcosf@users.noreply.github.com>
2025-10-26 12:04:26 -0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+> https://rust-lang.github.io/async-book/
+
+> https://tokio.rs/tokio/tutorial/spawning#tasks
+
 > CPU-bound computations in parallel: https://docs.rs/rayon/latest/rayon/
 > Blog post - Async, what is blocking?: https://ryhl.io/blog/async-what-is-blocking/#the-rayon-crate
+
+> https://github.com/pretzelhammer/rust-blog/blob/master/posts/common-rust-lifetime-misconceptions.md#2-if-t-static-then-t-must-be-valid-for-the-entire-program
--- src/my-mini-redis/src/main.rs
+++ src/my-mini-redis/src/main.rs
@@ -1,15 +1,32 @@
-use mini_redis::{client, Result};
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
+async fn process(socket: TcpStream) {
+    let mut conn = Connection::new(socket);
+
+    if let Some(frame) = conn.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+
+        let response = Frame::Error("unimplemented".to_string());
+        conn.write_frame(&response).await.unwrap();
+    }
+}
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    client.set("hello", "world".into()).await?;
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
 
-    let result = client.get("hello").await?;
+        let handle = tokio::spawn(async move {
+            // process each socket concurrently
+            process(socket).await;
+        });
 
-    println!("got value from redis: {:?}", result);
-
-    Ok(())
+        let out = handle.await;
+        if out.is_err() {
+            eprintln!("task failed: {:?}", out.err());
+        }
+    }
 }
